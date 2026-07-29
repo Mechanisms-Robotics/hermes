cf414b1f8e1624df3cd5919639e41c18588c8945
Nolan Martzall <Nolan.M@themechanisms.org>
2026-07-28 20:33:49 -0400

    added test_mech_colors and messsed with test_rgb

---
+++ src/led/test_mech_colors.py
@@ -0,0 +1,36 @@
+from machine import Pin
+from neopixel import NeoPixel
+import time
+
+num_leds = 80
+np = NeoPixel(Pin(2), num_leds)
+
+def clear_leds():
+    for i in range(num_leds):
+        set_led(i, off)
+    np.write()
+    
+def set_led(index, color):
+    r, g, b = color # unpack the array
+    np[index] = (g, r, b) # WS2811 LED is weird and requires GRB
+
+off = (0,0,0)
+
+gold = (50, 7, 0)
+white = (20, 20, 15)
+
+colors = [
+    gold,
+    white,
+    gold,
+    white
+    ]
+
+for i in range(len(colors)):
+    set_led(i, colors[i])
+    
+np.write()
+
+# wait 3 seconds, and then clear the leds
+time.sleep(3)
+clear_leds()
